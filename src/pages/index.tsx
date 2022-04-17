4840e8e9d5ea46d2843c5b9f91e839ac083c2058
gregory6z <gregoryrag@gmail.com>
2022-04-17 16:00:49 +0200

    eslint prettier

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,11 @@
+import { Flex } from "@chakra-ui/react";
+
 import type { NextPage } from "next";
 
-const Home: NextPage = () => {
-  return <h1>Dashboard</h1>;
-};
+const Home: NextPage = () => (
+  <Flex w="100vw" h="100vh" align="center" justify="center">
+    <Flex as="form" />
+  </Flex>
+);
 
 export default Home;

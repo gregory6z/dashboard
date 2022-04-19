d45e4291a2823b3b6587e93f8d24ce59d72b1e39
gregory6z <gregoryrag@gmail.com>
2022-04-19 13:07:42 +0200

    header responsive

--- src/components/Header/Logo.tsx
+++ src/components/Header/Logo.tsx
@@ -2,7 +2,11 @@ import { Text } from "@chakra-ui/react";
 
 export function Logo() {
   return (
-    <Text w="64" fontSize="3xl" fontWeight="bold" letterSpacing="tight">
+    <Text
+      w="64"
+      fontSize={["2xl", "3xl"]}
+      fontWeight="bold"
+      letterSpacing="tight">
       Dashboard
       <Text as="span" ml="1" color="pink.500">
         .

329a2ccfb49cdf2a59e36ec5927a4159df7f5d04
gregory6z <gregoryrag@gmail.com>
2022-04-21 13:32:17 +0200

    navigation flow

--- src/components/Sidebar/SidebarNav.tsx
+++ src/components/Sidebar/SidebarNav.tsx
@@ -13,13 +13,21 @@ export function SidebarNav() {
   return (
     <Stack spacing="12" align="flex-start">
       <NavSection title="GÉNÉRAL">
-        <NavLink icon={RiDashboardLine}>Dashboard</NavLink>
-        <NavLink icon={RiContactsLine}>Utilisateur</NavLink>
+        <NavLink icon={RiDashboardLine} href="/dashboard">
+          Dashboard
+        </NavLink>
+        <NavLink icon={RiContactsLine} href="users">
+          Utilisateur
+        </NavLink>
       </NavSection>
 
       <NavSection title="AUTOMATISATION">
-        <NavLink icon={RiInputMethodLine}>Formulaires</NavLink>
-        <NavLink icon={RiGitMergeLine}>Automatisation</NavLink>
+        <NavLink icon={RiInputMethodLine} href="/forms">
+          Formulaires
+        </NavLink>
+        <NavLink icon={RiGitMergeLine} href="/automation">
+          Automatisation
+        </NavLink>
       </NavSection>
     </Stack>
   );

56767d08ed679281ea950afeddf94c99162325de
gregory6z <gregoryrag@gmail.com>
2022-04-24 21:21:32 +0200

    signaling data refetch

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -24,7 +24,7 @@ import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
 
 export default function UserList() {
-  const { data, isLoading, error } = useQuery(
+  const { data, isLoading, error, isFetching } = useQuery(
     "users",
     async () => {
       const response = await fetch("http://localhost:3000/api/users");
@@ -64,6 +64,9 @@ export default function UserList() {
           <Flex mb="8" justify="space-between" align="center">
             <Heading size="lg" fontWeight="normal">
               Utilisateur
+              {!isLoading && isFetching && (
+                <Spinner size="sm" color="gray.500" ml="4" />
+              )}
             </Heading>
             <Link href="/users/create" passHref>
               <Button
